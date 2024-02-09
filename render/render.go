f58b66cfb21db627ea040ea88a9dc644a747f103
SandmanR <mredinger4@gmail.com>
2024-02-08 20:09:42 -0500

    Adding Jet engine support to the page rendering pkg...

--- render/render.go
+++ render/render.go
@@ -3,6 +3,7 @@ package render
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"strings"
 
@@ -36,11 +37,13 @@ func (h *Render) Page(w http.ResponseWriter, r *http.Request, view string, varia
 	case "go":
 		return h.GoPage(w, r, view, data)
 	case "jet":
+		return h.JetPage(w, r, view, variables, data)
 
 	}
 	return nil
 }
 
+// GoPage renders a standard Go template...
 func (h *Render) GoPage(w http.ResponseWriter, r *http.Request, view string, data interface{}) error {
 	tmpl, err := template.ParseFiles(fmt.Sprintf("%s/views/%s.page.tmpl", h.RootPath, view))
 	if err != nil {
@@ -57,5 +60,33 @@ func (h *Render) GoPage(w http.ResponseWriter, r *http.Request, view string, dat
 		return err
 	}
 
+	return nil
+}
+
+// JetPage renders a template using the Jet templating engine...
+func (h *Render) JetPage(w http.ResponseWriter, r *http.Request, templateName string, variables, data interface{}) error {
+	var vars jet.VarMap
+
+	if variables == nil {
+		vars = make(jet.VarMap)
+	} else {
+		vars = variables.(jet.VarMap)
+	}
+
+	td := &TemplateData{}
+	if data != nil {
+		td = data.(*TemplateData)
+	}
+
+	t, err := h.JetViews.GetTemplate(fmt.Sprintf("%sjet", templateName))
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	if err = t.Execute(w, vars, td); err != nil {
+		log.Println(err)
+		return err
+	}
 	return nil
 }
