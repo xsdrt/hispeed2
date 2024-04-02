3b3707c7aac491316c0190d17bc9e08986a26d7f
SandmanR <mredinger4@gmail.com>
2024-04-01 20:19:11 -0400

    Little improements to the make auth function...

--- cmd/cli/auth.go
+++ cmd/cli/auth.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+
 	"log"
 	"time"
+
+	"github.com/fatih/color"
 )
 
 func doAuth() error {
@@ -41,6 +44,23 @@ func doAuth() error {
 	if err != nil {
 		exitGracefully(err)
 	}
+
+	// copy over middleware
+	err = copyFileFromTemplate("templates/middleware/auth.go.txt", his.RootPath+"/middleware/auth.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/middleware/auth-token.go.txt", his.RootPath+"/middleware/auth-token.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	color.Yellow(" - users, tokens, and remember_tokens migrations created and executed")
+	color.Yellow(" - user and token models created")
+	color.Yellow(" - auth middleware created")
+	color.Yellow("")
+	color.Yellow("Don't forget to add user and token models in data/models.go, and to add appropiate middleware to your routes!")
 
 	return nil
 }
